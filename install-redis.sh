846b5e4b0683a382dc07320676f2c60185c4773a
Matt Hornung <hornung.m1@gmail.com>
2019-10-26 12:18:39 -0500

    update

--- install-redis.sh
+++ install-redis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #helm install --atomic --set password=changeit -f values.yaml redis-release stable/redis
-helm install --atomic -f values.yaml redis-release stable/redis
+helm upgrade --atomic -f values.yaml redis-release stable/redis
